3a4b9ce054be276b67430cc1ddcd1cf95ea632c7
Vitor Moraes <vitor.tm@gmail.com>
2025-11-12 18:41:06 -0300

    Commit inicial

---
+++ src/test/java/com/vitormoraes/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitormoraes.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
