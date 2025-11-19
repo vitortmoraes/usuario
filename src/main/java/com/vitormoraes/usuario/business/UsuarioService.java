83c9daed7b7e78a9a33751274cce84244a73f561
Vitor Moraes <vitor.tm@gmail.com>
2025-11-19 15:18:57 -0300

    adicionado put de endereco e telefone

--- src/main/java/com/vitormoraes/usuario/business/UsuarioService.java
+++ src/main/java/com/vitormoraes/usuario/business/UsuarioService.java
@@ -1,10 +1,16 @@
 package com.vitormoraes.usuario.business;
 
 import com.vitormoraes.usuario.business.converter.UsuarioConverter;
+import com.vitormoraes.usuario.business.dto.EnderecoDTO;
+import com.vitormoraes.usuario.business.dto.TelefoneDTO;
 import com.vitormoraes.usuario.business.dto.UsuarioDTO;
+import com.vitormoraes.usuario.infrastructure.entity.Endereco;
+import com.vitormoraes.usuario.infrastructure.entity.Telefone;
 import com.vitormoraes.usuario.infrastructure.entity.Usuario;
 import com.vitormoraes.usuario.infrastructure.exceptions.ConflictException;
 import com.vitormoraes.usuario.infrastructure.exceptions.ResourceNotFoundException;
+import com.vitormoraes.usuario.infrastructure.repository.EnderecoRepository;
+import com.vitormoraes.usuario.infrastructure.repository.TelefoneRepository;
 import com.vitormoraes.usuario.infrastructure.repository.UsuarioRepository;
 import com.vitormoraes.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +26,8 @@ public class UsuarioService {
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
+    private final EnderecoRepository enderecoRepository;
+    private final TelefoneRepository telefoneRepository;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
         emailExiste(usuarioDTO.getEmail());
@@ -43,9 +51,13 @@ public class UsuarioService {
         return usuarioRepository.existsByEmail(email);
     }
 
-    public Usuario buscarUsuarioPorEmail(String email){
-        return usuarioRepository.findByEmail(email).orElseThrow
-                (() -> new ResourceNotFoundException("Email não encontrado." + email));
+    public UsuarioDTO buscarUsuarioPorEmail(String email) {
+        try {
+            return usuarioConverter.paraUsuarioDTO(usuarioRepository.findByEmail(email)
+                    .orElseThrow(() -> new ResourceNotFoundException("Email não encontrado." + email)));
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("Email não encontrado." + email);
+        }
     }
 
     public void deletaUsuarioPorEmail(String email){
@@ -69,5 +81,23 @@ public class UsuarioService {
 
         //Salvou os dados do usuário convertido e depois pegou o retorno e converteu para usuarioDTO
         return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+    }
+
+    public EnderecoDTO atualizaEndereco(Long idEndereco, EnderecoDTO enderecoDTO){
+
+        Endereco entity = enderecoRepository.findById(idEndereco).orElseThrow(() ->
+                new ResourceNotFoundException("Id não encontrado." + idEndereco));
+
+        Endereco endereco = usuarioConverter.updateEndereco(enderecoDTO, entity);
+        return usuarioConverter.paraEnderecoDTO(enderecoRepository.save(endereco));
+    }
+
+    public TelefoneDTO atualizaTelefone(Long idTelefone, TelefoneDTO dto){
+
+        Telefone entity = telefoneRepository.findById(idTelefone).orElseThrow(() ->
+                new ResourceNotFoundException("Id não encontrado." + idTelefone));
+
+        Telefone telefone = usuarioConverter.updtadeTelefone(dto, entity);
+        return usuarioConverter.paraTelefoneDTO(telefoneRepository.save(telefone));
     }
 }
--- src/main/java/com/vitormoraes/usuario/controller/UsuarioController.java
+++ src/main/java/com/vitormoraes/usuario/controller/UsuarioController.java
@@ -2,6 +2,8 @@ package com.vitormoraes.usuario.controller;
 
 
 import com.vitormoraes.usuario.business.UsuarioService;
+import com.vitormoraes.usuario.business.dto.EnderecoDTO;
+import com.vitormoraes.usuario.business.dto.TelefoneDTO;
 import com.vitormoraes.usuario.business.dto.UsuarioDTO;
 import com.vitormoraes.usuario.infrastructure.entity.Usuario;
 import com.vitormoraes.usuario.infrastructure.security.JwtUtil;
@@ -46,7 +48,7 @@ public class UsuarioController {
     }
 
     @GetMapping
-    public ResponseEntity<Usuario> buscaUsuarioPorEmail(@RequestParam("email") String email){
+    public ResponseEntity<UsuarioDTO> buscaUsuarioPorEmail(@RequestParam("email") String email){
         return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email)); //vai converter para uma resposta http
     }
 
@@ -60,5 +62,17 @@ public class UsuarioController {
     public ResponseEntity<UsuarioDTO> atualizaDadosUsuario(@RequestBody UsuarioDTO dto,
                                                            @RequestHeader("Authorization") String token){
         return ResponseEntity.ok(usuarioService.atualizaDadosUsuario(token, dto));
+    }
+
+    @PutMapping("/endereco")
+    public ResponseEntity<EnderecoDTO> atualizaEndereco(@RequestBody EnderecoDTO dto,
+                                                        @RequestParam("id") Long id){
+        return ResponseEntity.ok(usuarioService.atualizaEndereco(id, dto));
+    }
+
+    @PutMapping("/telefone")
+    public ResponseEntity<TelefoneDTO> atualizaTelefone(@RequestBody TelefoneDTO dto,
+                                                        @RequestParam("id") Long id){
+        return ResponseEntity.ok(usuarioService.atualizaTelefone(id, dto));
     }
 }
